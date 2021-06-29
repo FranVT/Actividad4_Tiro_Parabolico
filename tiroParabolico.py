87833936cc2b655dcb27527c7142f4f383f0d527
FranVT <fjrvazqueztf@gmail.com>
2021-06-29 10:35:27 -0500

    Tiro parabolico modificado
    Se aumenta la velocidad al movimeinto de los objetivos y del proyectil.
    
    Cuando un objetivo llega al límite izquierdo, se regrasa al límite derecho, creando un bucle.

--- tiroParabolico.py
+++ tiroParabolico.py
@@ -1,7 +1,9 @@
+""" Import functions """
 from random import randrange
 from turtle import *
 from freegames import vector
 
+""" Initial Conditions """
 ball = vector(-200, -200)
 speed = vector(0, 0)
 targets = []
@@ -11,8 +13,8 @@ def tap(x, y):
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 25
-        speed.y = (y + 200) / 25
+        speed.x = (x + 200) / 20 # Increase the velocity of the ball from 1/25 to 1/20
+        speed.y = (y + 200) / 20
 
 def inside(xy):
     "Return True if xy within screen."
@@ -23,10 +25,12 @@ def draw():
     clear()
 
     for target in targets:
+        """ Move the blue targets """
         goto(target.x, target.y)
         dot(20, 'blue')
 
     if inside(ball):
+        """ Return to home """
         goto(ball.x, ball.y)
         dot(6, 'red')
 
@@ -39,25 +43,27 @@ def move():
         target = vector(200, y)
         targets.append(target)
 
+    " Move the target to the left "
     for target in targets:
-        target.x -= 0.5
+        if target.x < -200:
+            " When hits the left edge, it goes to the right edge "
+            target.x = 200
+        else:
+            target.x -= 0.7
 
     if inside(ball):
         speed.y -= 0.35
-        ball.move(speed)
+        ball.move(speed) # Sum the components of the vectors ball with speed
 
     dupe = targets.copy()
     targets.clear()
 
     for target in dupe:
-        if abs(target - ball) > 13:
+        if abs(target - ball) > 13: # 13 it is the radius of the target
             targets.append(target)
 
+    """ Create the canvas """
     draw()
-
-    for target in targets:
-        if not inside(target):
-            return
 
     ontimer(move, 50)
 
